2f456a422e792e116e098c52b011218da6844c26
Shehbaz Ali <shahmainaqpa@gmail.com>
2023-03-20 19:35:00 +0500

    Fourteenth Change

--- 10th.js
+++ 10th.js
@@ -2,3 +2,29 @@ function showMessage() {
     console.log( 'Hello everyone!' );
   }
   showMessage();
+  let a="ali";
+  let b=2;
+  if(a===b)
+  {
+    console.log("value are identical ")
+  }
+else {
+ console.log("value are notidentical ")
+}
+ let x=4;
+  let y=2;
+  if(x!=y)
+  {
+    console.log("Numbers are not equal ")
+  }
+else {
+ console.log("Numbers are not equal  ")
+}
+let c=2;
+let d= 4;
+if(c!==d){
+  console.log(false)
+}
+else{
+  console.log(true);
+}
