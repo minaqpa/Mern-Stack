a7fc99a05cbf6c7ba86c05e2e454a72fbb001378
Shehbaz Ali <shahmainaqpa@gmail.com>
2023-03-20 20:10:34 +0500

    Fifteenth Change

--- 10th.js
+++ 10th.js
@@ -23,8 +23,33 @@ else {
 let c=2;
 let d= 4;
 if(c!==d){
-  console.log(false)
+  console.log(true)
 }
 else{
-  console.log(true);
+  console.log(false);
 }
+let t=3;
+let u=5;
+let i=t+u;
+console.log("The sum of 5 and 3 is "+i);
+let f = 5;
+let h = 2;
+let k = f % h;
+console.log("Modulus of f and h is "+k);
+let q = 5;
+let w = 2;
+let r = f / h;
+console.log("the Division of q and w is "+r);
+let uu = 5;
+uu++;
+let o = uu;
+console.log(o);
+
+let tt = 5;
+tt--;
+let p = tt;
+console.log(p);
+let aa = 5;
+let yy = aa ** 2;
+console.log(yy);
+
