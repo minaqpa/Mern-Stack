bad6ed9ccf579a9cf00ab34923c0c6e8d9be0c84
Shehbaz Ali <shahmainaqpa@gmail.com>
2023-03-22 20:10:48 +0500

    array and function

---
+++ array.js
@@ -0,0 +1,8 @@
+// let car=["BMW","Roll Royce"];
+// console.log(car);
+
+const fruit = ["banana","apple"];
+console.log(fruit);
+const val = fruit.toString();
+
+console.log(val)
---
+++ function.js
@@ -0,0 +1,8 @@
+function mul(v,c){
+    let v=2;
+    let c=7
+    let res=v*c;
+return res;
+
+}
+console.log(res);
